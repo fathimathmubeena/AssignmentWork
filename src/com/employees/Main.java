a2d73d55dd81d04e56a55c09afed09e8333f4485
Fathimath Mubeena A <fathimathmubeena123@gmail.com>
2021-11-23 10:46:01 +0530

    changes done according to comments

---
+++ src/com/employees/Main.java
@@ -0,0 +1,10 @@
+package com.employees;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is the Employee Management project");
+        System.out.println("It contains 3 classes.\n1)Employee Class\n2)EmployeeService\n3)EmployeeController");
+    }
+}
