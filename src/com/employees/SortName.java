09309438fcbe1d5e847430b3167414d4bd998a80
Fathimath Mubeena A <fathimathmubeena123@gmail.com>
2021-11-23 12:10:12 +0530

    changes done according to comments

---
+++ src/com/employees/SortName.java
@@ -0,0 +1,9 @@
+package com.employees;
+
+import java.util.Comparator;
+
+public class SortName implements Comparator<Employee>{
+    public int compare(Employee emp1,Employee emp2){
+        return emp1.getName().compareTo(emp2.getName());
+    }
+}
